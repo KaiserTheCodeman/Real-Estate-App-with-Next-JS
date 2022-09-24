e6c61965f5a1f927731894d947bdc6cbdfa0afc6
furkancoder <mfurkankaraca97@gmail.com>
2022-09-24 16:59:03 +0300

    ilk commit

---
+++ components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Box } from '@chakra-ui/react';
+
+
+const Footer = () =>  (
+    <Box textAlign="center" p="5" borderTop="1px" color="gray.600" borderColor="gray.100" >
+        2022, Realtor, Inc <br />
+        Made By Furkan Karaca 
+    </Box>
+  )
+
+
+export default Footer
